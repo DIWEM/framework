04082ff87a9186c208b383527c74a7b8efbb097b
Mauricio G <mau.gar.gonz@icloud.com>
2018-04-09 15:19:22 -0500

    tabs y variables

---
+++ js/configuracion.js
@@ -0,0 +1,17 @@
+// Configuracion basica
+var nombre_de_la_aplicacion = "Framework";
+var color_primario = "ingigo";
+var color_secundario = "amber";
+var boton_accion = "Boton";
+
+//Configuracion de Barra lateral
+
+// Configuracion de menu secundario
+var menu_uno = "Uno";
+var icono_menu_uno = "Uno";
+
+var menu_dos = "Dos";
+var icono_menu_dos = "Dos";
+
+var menu_tres = "Tres";
+var icono_menu_tres = "Tres";
